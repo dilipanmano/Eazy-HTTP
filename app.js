0614473c6a8c689483f54fa382e4429bfa353689
Dilipan M <dilipan12m@gmail.com>
2022-09-07 00:01:59 +0530

    post method

--- app.js
+++ app.js
@@ -9,11 +9,25 @@ const eazy = new eazyHttp;
 //     }
 // });
 
-//GET Method -Single Record
-eazy.get('https://jsonplaceholder.typicode.com/posts/1',function(err,response){
+// //GET Method -Single Record
+// eazy.get('https://jsonplaceholder.typicode.com/posts/1',function(err,response){
+//     if(err){
+//         console.log("error "+err);
+//     }else{
+//         console.log(response);
+//     }
+// });
+
+//POST Method
+let data = {
+    'title':'Content',
+    'body':'This is a body'
+}
+eazy.post('https://jsonplaceholder.typicode.com/posts',data,function(err,response){
     if(err){
         console.log("error "+err);
     }else{
         console.log(response);
     }
 });
+
--- eazyHttp.js
+++ eazyHttp.js
@@ -2,6 +2,7 @@ function eazyHttp(){
     this.http = new XMLHttpRequest();
 }
 
+//GET Method
 eazyHttp.prototype.get = function(url,callback){
     this.http.open('GET',url,true);
     let self = this;
@@ -13,4 +14,15 @@ eazyHttp.prototype.get = function(url,callback){
         }
     }
     this.http.send();
+}
+
+//POST Method
+eazyHttp.prototype.post = function(url,data,callback){
+    this.http.open('POST',url,true);
+    this.http.setRequestHeader('content-type','application/json');
+    let self = this;
+    this.http.onload = function(){
+        callback(null, self.http.responseText);
+    }
+    this.http.send(JSON.stringify(data));
 }
